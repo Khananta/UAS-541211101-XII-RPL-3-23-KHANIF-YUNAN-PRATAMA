13899076a17b02ed0f0ec402c5842b2a6de4cd03
Khananta <khanantaaa@gmail.com>
2023-11-24 20:55:01 +0700

    UAS1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3000
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.send('Ini adalah tugas UAS 23_541211101_Khanif Yunan Pratama!')
 })
 
